e954c102757132992130d0611da71a2f9ce89ed9
Kwon JungHwan <oke2103@gmail.com>
2021-03-07 23:40:21 +0900

    03/07

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -1,6 +1,7 @@
 package hello.core.xml;
 
 import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
@@ -10,9 +11,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class XmlAppContext {
 
     @Test
+    @DisplayName("xml 설정으로 빈 조회")
     void xmlAppContext(){
-        Object ac = new GenericXmlApplicationContext("appConfig.xml");
-//        MemberService memberService = ac.getBean("memberService", MemberService.class);
-//        assertThat(memberService).isInstanceOf(MemberService.class);
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
     }
 }
